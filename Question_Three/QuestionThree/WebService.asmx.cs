c31749f51528cd6739f86a2acddf966aaab94503
aaronmccormack15533 <maron.accormack@gmail.com>
2020-02-27 13:46:01 +0000

    Question Three

---
+++ Question_Three/QuestionThree/WebService.asmx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace QuestionThree
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+        // global instance of the file input
+        // three methods for each section
+        // return the response coddes
+        // have a webform to display each code
+        // display in the onload
+
+        [WebMethod]
+        public int CorrectData()
+        {
+            var file = "D:\\Projects\\FRS\\Question_Three\\QuestionThree\\InputDocument.xml";
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var xmlFilePath = Path.Combine(currentDirectory, file);
+
+            XDocument xml = XDocument.Load(file);
+
+            
+
+            if (!CheckAttr(xml))
+            {
+                return -1;
+            }
+            else if(!CheckSiteId(xml))
+            {
+                return -2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool CheckAttr(XDocument xml)
+        {
+            var result = (from q in xml.Descendants("Declaration")
+                          select (string)q.Attribute("Command"));
+
+
+
+            foreach (var res in result)
+            {
+                if (result.Contains("DEFAULT"))
+                {
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+
+                }                 
+            }
+            return false;
+        }
+
+        public bool CheckSiteId(XDocument xml)
+        {
+            var result = from q in xml.Descendants("DeclarationHeader")
+                          select q.Element("SiteID").Value;
+
+            foreach(var res in result)
+            {
+                System.Diagnostics.Debug.WriteLine(res.Contains("DUB"));
+                if (res.Contains("DUB"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        
+
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
